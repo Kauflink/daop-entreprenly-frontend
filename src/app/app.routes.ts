2a97fc693b9468c978019b9a06a9480b7bac6c0a
Juyens <joseph.juliuscb@gmail.com>
2026-05-09 21:27:47 -0500

    feat(core): implement base interfaces, shared components and main views
    Added:
    - Base API, Endpoint, Assembler, Entity, and Response interfaces.
    - Shared LanguageSwitcher and Layout components.
    - Home, PageNotFound, and Help views.
    - Global routing configuration.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './shared/presentation/views/home/home';
+
+const help = () => import('./shared/presentation/views/help/help').then((m) => m.Help);
+const pageNotFound = () => import('./shared/presentation/views/page-not-found/page-not-found').then((m) => m.PageNotFound);
+const baseTitle = 'Entreprenly';
+
+export const routes: Routes = [
+  { path: 'home', component: Home, title: `${baseTitle} - Home` },
+  { path: 'help', loadComponent: help, title: `${baseTitle} - Help` },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not Found` },
+];
