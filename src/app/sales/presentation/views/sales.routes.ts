e8047a7512ef56aa0249dda2230dfe79b9682233
Ferdinant12-ops <darkh061@gmail.com>
2026-05-10 02:32:50 -0500

    salesActualizado

--- src/app/sales/presentation/views/sales.routes.ts
+++ src/app/sales/presentation/views/sales.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 
-const salesPage = () => import('./sales-page/sales-page').then(m => m.SalesPage);
+const salesPage = () => import('./sales-page/sales-page').then((m) => m.SalesPage);
 
-export const salesRoutes: Routes = [
-  { path: '', loadComponent: salesPage }
-];
+export const salesRoutes: Routes = [{ path: '', loadComponent: salesPage }];
