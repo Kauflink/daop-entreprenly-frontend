802c9949e3b2e58b4835dc09e47b607b3cd6e08b
Juyens <joseph.juliuscb@gmail.com>
2026-05-11 22:51:41 -0500

    feat(i18n): translate browser tab titles based on active language

--- src/app/profile/presentation/profile.routes.ts
+++ src/app/profile/presentation/profile.routes.ts
@@ -4,5 +4,6 @@ export const profileRoutes: Routes = [
   {
     path: '',
     loadComponent: () => import('./views/profile-page/profile-page').then((m) => m.ProfilePage),
+    title: 'pageTitle.profile',
   },
 ];
